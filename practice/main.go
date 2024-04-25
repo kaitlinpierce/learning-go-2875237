9f464416d2b42cdde126c580b0719a6b3d13644d
Kaitlin Pierce <2022pierce@gmail.com>
2024-04-25 13:27:25 -0500

    Finish

--- practice/main.go
+++ practice/main.go
@@ -8,6 +8,19 @@ import (
 
 func main() {
 	rand.Seed(time.Now().Unix())
-	dow := rand.Intn(7) + 1
-	fmt.Println("Day", dow)
+
+	// fmt.Println("Day", dow)
+
+	var result string
+	switch dow := rand.Intn(7) + 1; dow {
+	case 1:
+		result = "Sunday"
+		// fallthrough
+	case 2:
+		result = "Monday"
+		// fallthrough
+	default:
+		result = "Other"
+	}
+	fmt.Println(result)
 }
